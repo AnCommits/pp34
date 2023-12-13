ecf1641a4969e392e4344d2fdf640173ec6d1077
AnUser99 <kisamasyanya@gmail.com>
2023-12-13 17:41:10 +0300

    -

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -9,6 +9,7 @@ async function adminPage(myId) {
         })
 
         const allRoles = await loadAllRoles()
+        createTrTagsForRoles(allRoles)
         document.getElementById('roles_number').textContent = allRoles.length
         createOptionTags('new_user_roles', allRoles)
         createOptionTags('modal_roles', allRoles)
@@ -213,4 +214,30 @@ function createOptionTags(parentSelectId, allRoles) {
         option.hidden = false
         select.appendChild(option)
     }
+}
+
+function createTrTagsForRoles(allRoles) {
+    for (let i in allRoles) {
+        const newTr = document.createElement('tr')
+        newTr.setAttribute('class', 'right_block_role')
+        newTr.setAttribute('id', 'right_block_role_' + allRoles[i])
+        // console.log(newTr)
+
+        // const option = option_template.cloneNode(true)
+        // option.id = 'option_' + parentSelectId + '_' + i
+        // option.textContent = allRoles[i]
+        // option.hidden = false
+        // select.appendChild(option)
+    }
+
+    // newTr.setAttribute('id', 'right_block_role_' + allRoles.id)
+    // newTr.innerHTML = document.getElementById('right_block_role_tr_template').innerHTML
+    //     .replaceAll('template', user.id.toString())
+    // document.getElementById('right_block_users').appendChild(newTr)
+    //
+    // document.getElementById('user_id_' + user.id).textContent = user.id.toString()
+    // document.getElementById('user_firstname_' + user.id).textContent = user.firstname
+
+    // const userHasRoleAdmin = putRolesIntoLiTagsAndCheckAdmin('user_roles_' + user.id, user)
+    // }
 }
